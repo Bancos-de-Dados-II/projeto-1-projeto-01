2754d0a411744343b7ffe9a4481c8e04395cd41c
marianemirelii <mariane.mireli@academico.ifpb.edu.br>
2025-12-02 19:52:45 -0300

    Feat: Adicionando rota para atualizar

--- src/routes/institution-routes.ts
+++ src/routes/institution-routes.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import registerInstitutionController from '../controllers/register-institution-controller.js';
 import getAllInstitutionsController from '../controllers/getAll-institutions-controller.js';
 import deleteInstitutionController from '../controllers/delete-institution-controller.js'; 
+import updateInstitutionController from '../controllers/update-institution-controller.js';
+
 
 const intitutionRoutes = express.Router();
 
 intitutionRoutes.get('/', getAllInstitutionsController.handle);
 intitutionRoutes.post('/', registerInstitutionController.handle);
-intitutionRoutes.delete('/:id', deleteInstitutionController.handle); 
+intitutionRoutes.delete('/:id', deleteInstitutionController.handle);
+intitutionRoutes.put('/:id', updateInstitutionController.handle);
 
 export default intitutionRoutes;
