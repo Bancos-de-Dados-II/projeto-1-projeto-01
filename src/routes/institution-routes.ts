0a7b5dd61fb49240e697b2c09a9f8e33de7041cb
marianemirelii <mariane.mireli@academico.ifpb.edu.br>
2025-12-02 21:33:32 -0300

    Feat: Adicionando rota para busca por id

--- src/routes/institution-routes.ts
+++ src/routes/institution-routes.ts
@@ -3,7 +3,7 @@ import registerInstitutionController from '../controllers/register-institution-c
 import getAllInstitutionsController from '../controllers/getAll-institutions-controller.js';
 import deleteInstitutionController from '../controllers/delete-institution-controller.js'; 
 import updateInstitutionController from '../controllers/update-institution-controller.js';
-
+import GetInstitutionByIdController from '../controllers/get-by-id-institution-controller.js';
 
 const intitutionRoutes = express.Router();
 
@@ -11,5 +11,6 @@ intitutionRoutes.get('/', getAllInstitutionsController.handle);
 intitutionRoutes.post('/', registerInstitutionController.handle);
 intitutionRoutes.delete('/:id', deleteInstitutionController.handle);
 intitutionRoutes.put('/:id', updateInstitutionController.handle);
+intitutionRoutes.get('/:id', GetInstitutionByIdController.handle);
 
 export default intitutionRoutes;
